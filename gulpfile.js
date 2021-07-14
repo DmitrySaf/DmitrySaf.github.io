6ecca2a657799a5eef0758243b575658738ba616
Dmitry <dmitry.study@yandex.ru>
2021-07-14 15:22:19 +0300

    gulp setup

--- gulpfile.js
+++ gulpfile.js
@@ -3,30 +3,71 @@ const gulp = require('gulp'),
  	cleanCSS = require('gulp-clean-css'),
 	autoprefixer = require('gulp-autoprefixer'),
 	rename = require("gulp-rename"),
-	browserSync = require('browser-sync');
+	browserSync = require('browser-sync'),
+	htmlmin = require('gulp-htmlmin'),
+	uglify = require('gulp-uglify'),
+	imagemin = require('gulp-imagemin');
 
-gulp.task('sass', function(){ // Создаем таск Sass
-	return gulp.src('src/sass/**/*.sass') // Берем источник
-		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError)) // Преобразуем Sass в CSS посредством gulp-sass
-		.pipe(rename({suffix: '.min', prefix: ''}))
-		.pipe(autoprefixer())
-		.pipe(cleanCSS({compatibility: 'ie8'}))
-		.pipe(gulp.dest('src/css')) // Выгружаем результата в папку app/css
-		.pipe(browserSync.stream()); // Обновляем CSS на странице при изменении
+gulp.task('server', function() {
+
+    browserSync({
+        server: {
+            baseDir: "dist"
+        }
+    });
+
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('styles', function() {
+    return gulp.src("src/sass/**/*.+(scss|sass)")
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(autoprefixer())
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest("dist/css"))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
-    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('sass'));
+    gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
+    gulp.watch("src/*.html").on('change', gulp.parallel('html'));
+    gulp.watch("src/js/**/*.js").on('change', gulp.parallel('scripts'));
+    gulp.watch("src/fonts/**/*").on('all', gulp.parallel('fonts'));
+    gulp.watch("src/icons/**/*").on('all', gulp.parallel('icons'));
+    gulp.watch("src/img/**/*").on('all', gulp.parallel('images'));
 });
 
-gulp.task('browser-sync', function() { 
-	browserSync({ 
-		server: {
-			baseDir: 'src'
-		},
-		notify: false
-	});
-	gulp.watch("src/*.html").on('change', browserSync.reload);
+gulp.task('html', function () {
+    return gulp.src("src/*.html")
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest("dist/"));
 });
 
-gulp.task('default', gulp.parallel('sass', 'browser-sync', 'watch'));
+gulp.task('scripts', function () {
+    return gulp.src("src/js/**/*.js")
+		.pipe(uglify())
+        .pipe(gulp.dest("dist/js"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('fonts', function () {
+    return gulp.src("src/fonts/**/*")
+        .pipe(gulp.dest("dist/fonts"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('icons', function () {
+    return gulp.src("src/icons/**/*")
+        .pipe(gulp.dest("dist/icons"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('images', function () {
+    return gulp.src("src/img/**/*")
+        .pipe(imagemin())
+        .pipe(gulp.dest("dist/img"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'fonts', 'icons', 'html', 'images'));
