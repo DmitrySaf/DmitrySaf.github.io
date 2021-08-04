a35a6da25f70cbf767a664aa04582800a185b627
Dmitry <dmitry.study@yandex.ru>
2021-08-04 12:51:30 +0300

    made several cards

--- src/pug/components/cards/cards.js
+++ src/pug/components/cards/cards.js
@@ -1,3 +1,5 @@
 import './cards.sass';
 import './room-book/room-book.sass';
 import './registration/registration.sass';
+import './room-preview/room-preview.sass';
+import './room-preview/room-preview';
