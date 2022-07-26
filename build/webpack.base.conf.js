71c6b32f4de6fe62a7d2829b4aa6fb4b53b68ab3
DmitrySaf <dmitry.study@yandex.ru>
2022-07-26 17:52:18 +0300

    edited files paths

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -26,9 +26,8 @@ module.exports = {
 		app: PATHS.src
 	},
 	output: {
-		filename: `${PATHS.assets}js/[name].js`,
-		path: PATHS.dist,
-		publicPath: "/"
+		filename: `assets/js/[name].js`,
+		path: PATHS.dist
 	},
 	target: process.env.NODE_ENV === "development" ? "web" : "browserslist",
 	module: {
@@ -51,14 +50,14 @@ module.exports = {
 			test: /\.(ttf|eot|woff|woff2)$/,
 			type: 'asset/resource',
 			generator: {
-				filename: 'assets/fonts/[name].[ext]'
+				filename: 'assets/fonts/[name][ext]'
 			}
 			},
 			{
 			test: /\.(svg|png|jpg)$/,
 			type: 'asset/resource',
 			generator: {
-				filename: 'assets/img/[name].[ext]'
+				filename: 'assets/img/[name][ext]'
 			}
 			},
 			{
@@ -115,13 +114,14 @@ module.exports = {
 					from: `${PATHS.src}/${PATHS.assets}icons`,
 					to: `${PATHS.assets}icons`
 				}
-		]}),
+			]
+		}),
 		new MiniCssExtractPlugin({
-			filename: `blob/gh-pages/${PATHS.assets}css/[name].css`
+			filename: `assets/css/[name].css`
 		}),
 		new FaviconsWebpackPlugin({
 			logo: './src/assets/icons/logo.svg',
-			outputPath: './assets/favicon/',
+			outputPath: 'assets/favicon/',
 			prefix: 'assets/favicon/',
 		}),
 		...PAGES.map(filename => new HtmlWebpackPlugin({
