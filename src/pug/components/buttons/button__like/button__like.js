b47faab9f9e008a2e1a081426e19268e567bcf33
DmitrySaf <dmitry.study@yandex.ru>
2022-07-26 20:08:24 +0300

    reworked button__like and files paths

--- src/pug/components/buttons/button__like/button__like.js
+++ src/pug/components/buttons/button__like/button__like.js
@@ -1,1 +1,18 @@
 import './button__like.sass';
+
+const likeButton = $('.like');
+
+likeButton.on('click', (e) => {
+    const button = $(e.currentTarget),
+          buttonIcon = button.first(),
+          buttonText = button.last();
+    let counter = +likeButtonText.textContent;
+
+    button.toggleClass('like_active');
+    buttonIcon
+        .text(likeButtonIcon.textContent == 'favorite_border' ? 'favorite': 'favorite_border')
+        .toggleClass('material-icons_border');
+    buttonText
+        .text(!likeButtonText.classList.contains('like__counter_active') ? ++counter: --counter)
+        .toggleClass('like__counter_active');
+});
--- src/pug/components/buttons/buttons.js
+++ src/pug/components/buttons/buttons.js
@@ -1,28 +1,8 @@
 import './button__checkbox/button__checkbox';
 import './button__default/button__default';
 import './button__like/button__like';
-import './button__radio/button__radio.sass';
-import './button__rating/button__rating.sass';
-import './button__toggle/button__toggle.sass';
-import './button__next/button__next.sass';
+import './button__next/button__next';
+import './button__radio/button__radio';
 import './button__range/button__range';
-
-
-
-
-// js for like button
-
-$('.like').on('click', (e) => {
-    const likeButton = e.currentTarget,
-          likeButtonIcon = likeButton.firstElementChild,
-          likeButtonText = likeButton.lastElementChild;
-    
-    likeButton.classList.toggle('like_active');
-
-    likeButtonIcon.textContent = (likeButtonIcon.textContent == 'favorite_border' ? 'favorite': 'favorite_border');
-    likeButtonIcon.classList.toggle('material-icons_border');
-
-    let counter = +likeButtonText.textContent;
-    likeButtonText.textContent = (!likeButtonText.classList.contains('like__counter_active') ? ++counter: --counter);
-    likeButtonText.classList.toggle('like__counter_active');
-});
+import './button__rating/button__rating';
+import './button__toggle/button__toggle';
