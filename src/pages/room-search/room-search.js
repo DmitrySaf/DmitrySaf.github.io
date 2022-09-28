73715c474d968fca14e0c4fb5687b5470361e922
DmitrySaf <dmitry.study@yandex.ru>
2022-09-28 20:53:41 +0300

    connected block names in sass files #3

--- src/pages/room-search/room-search.js
+++ src/pages/room-search/room-search.js
@@ -3,8 +3,8 @@ const $filters = $('.js-room-search__filters');
 const $arrow = $('.js-room-search__arrow');
 
 $filtersTrigger.on('click', () => {
-  $filters.addClass('room-search__filters_show');
+  $filters.addClass('room-search__filters_shown');
   $arrow.on('click', () => {
-    $filters.removeClass('room-search__filters_show');
+    $filters.removeClass('room-search__filters_shown');
   });
 });
