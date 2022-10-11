f7fb8dfd09b6a52d15380e58df83379309cd0f14
DmitrySaf <dmitry.study@yandex.ru>
2022-10-11 21:24:03 +0300

    replaced styles sass with autoimport #3

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import './assets/sass/styles.sass';
-
 function importAll(resolve) {
   resolve.keys().forEach(resolve);
 }
 
+importAll(require.context('./assets/sass', true, /\.(sass)$/));
 importAll(require.context('./components', true, /\.(js)$/));
 importAll(require.context('./pages', true, /\.(js)$/));
 importAll(require.context('./components', true, /\.(sass)$/));
