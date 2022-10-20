a5d1750c752ee004bdf4cc62b57d53ad4f190d46
DmitrySaf <dmitry.study@yandex.ru>
2022-10-20 17:42:12 +0300

    renamed header menu element and changed styles #1

--- src/components/header-menu/header-menu.js
+++ src/components/header-menu/header-menu.js
@@ -1,4 +1,4 @@
-const $menuLinkCheckbox = $('.menu__link-checkbox');
+const $menuLinkCheckbox = $('.header-menu__link-checkbox');
 
 $menuLinkCheckbox.on('click', function () {
   $menuLinkCheckbox.not(this).prop('checked', false);
